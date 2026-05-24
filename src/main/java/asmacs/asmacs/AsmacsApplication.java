0ae3e933bc53e37b830cb0c6548c36b88ec099f5
iram2004 <iramnanda218@gmail.com>
2026-05-24 16:05:45 +0100

    premier commit

---
+++ src/main/java/asmacs/asmacs/AsmacsApplication.java
@@ -0,0 +1,13 @@
+package asmacs.asmacs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmacsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmacsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/asmacs/asmacs/AsmacsApplicationTests.java
@@ -0,0 +1,13 @@
+package asmacs.asmacs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmacsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
